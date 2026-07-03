cead5d5338b8e1e7589f8f7ef6dc0bd74bf028fe
goncaloandresferreira-star <goncaloandresferreira@gmail.com>
2026-07-03 19:28:05 +0000

    Gonçalo Poker

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function () {
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let suit = ["suit-hearts", "suit-diamonds", "suit-spades", "suit-clubs"];
+  let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  let randomSuitNumber = Math.floor(Math.random() * suit.length);
+  document.getElementById('cardContent').innerHTML = cardNumber[randomCardNumber];
+  document.getElementById('theCard').className = "card " + suit[randomSuitNumber];
+}
+
+
+
+
